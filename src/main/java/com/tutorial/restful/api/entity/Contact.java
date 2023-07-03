1c9105e0f5a4c9c5d8eeb873e1c12e2bb0cc297a
Budhi Octaviansyah <budioct@gmail.com>
2023-07-03 16:19:43 +0700

    buat entity dengan kardinalitas relasi

---
+++ src/main/java/com/tutorial/restful/api/entity/Contact.java
@@ -0,0 +1,39 @@
+package com.tutorial.restful.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    private String phone;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+
+    @OneToMany(mappedBy = "contact")
+    private List<Address> addresses;
+
+}
